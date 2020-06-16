269509ab239f8af4f9aab1d8a7dc2cf9a842ed81
mr-cade <cadelpetersen@gmail.com>
2020-06-16 10:17:50 -0600

    added score variable, reset loop fix, check answer still not working

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var countdownDisplay = document.getElementById("countdown");
+var scoreDisplay = document.getElementById("score");
 var highscoreBtn = document.getElementById("highscore");
 var startBtn = document.getElementById("start");
 var question = document.getElementById("question");
@@ -6,7 +7,7 @@ var answer1 = document.getElementById("opt1");
 var answer2 = document.getElementById("opt2");
 var answer3 = document.getElementById("opt3");
 var answer4 = document.getElementById("opt4");
-
+var lastResultDisplay = document.getElementById("lastResult");
 var questions = [
     {
         question: "test",
@@ -31,27 +32,42 @@ var questions = [
         correct: "opt3"
     }
 ]
-
-var totalSeconds = 75;
+var totalSeconds = 60;
 var secondsElapsed = 0;
 var interval;
-var i = 0
+var i = 0;
+var score = 0;
 
 function startCountdown() {
         interval = setInterval(function() {
             var secondsLeft = totalSeconds - secondsElapsed;
+            if (secondsLeft >= 0) {
             secondsElapsed++;
             countdownDisplay.textContent = secondsLeft;
+            } else {
+                alert("Pencils Down!");
+                clearInterval(interval);
+                totalSeconds = 60;
+            }
         }, 1000);
     } 
 
-
 function renderQuestion() {
+    if (i < questions.length) {
         question.textContent = questions[i].question;
         answer1.textContent = questions[i].opt1;
         answer2.textContent = questions[i].opt2;
         answer3.textContent = questions[i].opt3;
         answer4.textContent = questions[i].opt4;
+    } else {
+        clearInterval(interval);
+        alert("Quiz Complete!");
+        totalSeconds = 60;
+        i = 0
+        document.getElementById("questionBlock").style.display = "none";
+        lastResultDisplay.textContent = "";
+
+    }
 }
 
 function startQuiz() {
@@ -61,14 +77,24 @@ function startQuiz() {
 }
 
 function checkAnswer() {
-    console.log(event.target);
-    if (event.target === questions.correct) {
-        alert("correct!")
+    console.log("event target: " + event.target)
+    console.log(questions[i].correct)
+    if (event.target.matches(questions[i].correct)) {
+        lastResultDisplay.textContent = "correct!";
+        score++;
+        console.log(score);
+        scoreDisplay.textContent = score;
     } else {
-        alert("incorrect :(")
+        lastResultDisplay.textContent = "incorrect :(";
+        score--;
+        scoreDisplay.textContent = score;
     }
     i++;
     renderQuestion();
+}
+
+function addHighscore() {
+
 }
 
 startBtn.addEventListener("click", startQuiz);
